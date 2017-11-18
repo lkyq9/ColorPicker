13e9f8da0d266cbe1082c1d9f738b5fbea98869f
Lydia Krasucki <lydiakrasucki@Lydias-MacBook-Pro.local>
2017-11-17 18:00:07 -0600

    First Commit

--- ColorPicker/ViewController.swift
+++ ColorPicker/ViewController.swift
@@ -8,8 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
 
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var colorpicker: UIPickerView!
+    
+    let colors = ["Red","Orange","Yellow","Green","Blue","Purple"]
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return colors[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        
+        return colors.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        label.text = colors[row]
+        
+        if (row == 0)
+        {
+            self.view.backgroundColor = UIColor.red;
+    
+        }
+    
+        else if ( row == 1)
+        {
+            self.view.backgroundColor = UIColor.orange;
+            
+        }
+    
+        else if (row == 2)
+        {
+            self.view.backgroundColor = UIColor.yellow;
+            
+        }
+    
+        else if (row == 3)
+        {
+            self.view.backgroundColor = UIColor.green;
+            
+        }
+    
+        else if (row == 4)
+        {
+            self.view.backgroundColor = UIColor.blue;
+            
+        }
+    
+        else
+        {
+            self.view.backgroundColor = UIColor.purple;
+            
+        }
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
